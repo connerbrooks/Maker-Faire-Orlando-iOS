8a6fcf07bbbf87e8a2d30e4bd5dd859b939b734a
Conner Brooks <cbrooks@Conners-MacBook-Pro.local>
2014-06-08 21:54:44 -0700

    Initial Commit

---
+++ Orlando Maker Faire/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Orlando Maker Faire
+//
+//  Created by Conner Brooks on 6/8/14.
+//  Copyright (c) 2014 Conner Brooks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Orlando Maker FaireTests/Orlando_Maker_FaireTests.swift
@@ -0,0 +1,35 @@
+//
+//  Orlando_Maker_FaireTests.swift
+//  Orlando Maker FaireTests
+//
+//  Created by Conner Brooks on 6/8/14.
+//  Copyright (c) 2014 Conner Brooks. All rights reserved.
+//
+
+import XCTest
+
+class Orlando_Maker_FaireTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
