b25db02f792870b3aa019a40b361818883cbf7ee
Conner Brooks <connerbrooks@gmail.com>
2014-06-10 23:24:15 -0700

    Add detail view for maker content

---
+++ Orlando Maker Faire/MakerDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MakerDetailViewController.swift
+//  Maker Faire Orlando
+//
+//  Created by Conner Brooks on 6/10/14.
+//  Copyright (c) 2014 Conner Brooks. All rights reserved.
+//
+
+import UIKit
+
+
+class MakerDetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
