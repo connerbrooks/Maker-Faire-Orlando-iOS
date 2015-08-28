e8ee10c3a2c2c89b8164cbaabe9c849db8873624
Dana Brooks <dana.brooks@unify.com>
2015-08-27 22:58:52 -0400

    Add segmented control - basically works

--- Orlando Maker Faire/EventViewController.swift
+++ Orlando Maker Faire/EventViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class EventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, EventAPIProtocol {
     
+    @IBOutlet weak var daySegmentedControl: UISegmentedControl!
     let kCellIdentifier: String = "EventCell"
     
     var api: EventAPI?
@@ -17,7 +18,7 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet var eventTableView : UITableView!
     
     
-    var events: [Event] = []
+    var events: [[Event]] = [[]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,7 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
         self.api = EventAPI(delegate: self)
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
         self.api?.getEvents()
+        self.daySegmentedControl.addTarget(self, action: Selector("handleSegment:"), forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,14 +35,14 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        return self.events[daySegmentedControl.selectedSegmentIndex].count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as! UITableViewCell
         
-        let event = self.events[indexPath.row]
+        let event = self.events[daySegmentedControl.selectedSegmentIndex][indexPath.row]
         cell.textLabel!.text = event.summary
         cell.detailTextLabel!.text = event.location
         
@@ -50,37 +52,41 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         var detailViewController: EventDetailViewController = segue.destinationViewController as! EventDetailViewController
         var eventIndex = eventTableView.indexPathForSelectedRow()!.row
-        var selectedEvent = self.events[eventIndex]
+        var selectedEvent = self.events[daySegmentedControl.selectedSegmentIndex][eventIndex]
         detailViewController.event = selectedEvent
         
     }
     
     func didReceiveAPIResults(results: NSDictionary) {
         let days: NSArray = results["days"] as! NSArray
-        let day: NSDictionary = days[0] as! NSDictionary
-        let date_title = day["date_title"] as! NSString
-        println("date_title = \(date_title)")
-        let events: [NSDictionary] = day["events"] as! [NSDictionary]
-        for e in events {
-            let name = e["name"] as! NSString
-            println("name = \(name)")
-            var event_description: String? = e["name"] as? String
-            var summary: String? = e["description"] as? String
-            var location: String? = e["location"] as? String
-            var link: String? = e["promo_url"] as? String
-
-            var startString : String = "1/1/90" // startDict["dateTime"] as! String
-            var endString : String = "1/2/91" // endDict["dateTime"] as! String
-
-            var formatter: NSDateFormatter = NSDateFormatter()
-            formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'-'HH':'mm'"
-
-            var start: NSDate = NSDate.new()
-            var end: NSDate = NSDate.new()
-            
-            var newEvent = Event(summary: summary, event_description: event_description, location: location, link: link, start: start, end: end)
-            
-            self.events.append(newEvent)
+        self.events.append([Event]())
+        for dayCounter in 0...1 {
+            let day: NSDictionary = days[dayCounter] as! NSDictionary
+            let date_title = day["date_title"] as! NSString
+            // daySegmentedControl.setTitle(day["date_title"] as! String!, forSegmentAtIndex: 0)
+            println("date_title = \(date_title)")
+            let ev: [NSDictionary] = day["events"] as! [NSDictionary]
+            for e in ev {
+                let name = e["name"] as! NSString
+                println("name = \(name)")
+                var event_description: String? = e["name"] as? String
+                var summary: String? = e["description"] as? String
+                var location: String? = e["location"] as? String
+                var link: String? = e["promo_url"] as? String
+                
+                var startString : String = "1/1/90" // startDict["dateTime"] as! String
+                var endString : String = "1/2/91" // endDict["dateTime"] as! String
+                
+                var formatter: NSDateFormatter = NSDateFormatter()
+                formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'-'HH':'mm'"
+                
+                var start: NSDate = NSDate.new()
+                var end: NSDate = NSDate.new()
+                
+                var newEvent = Event(summary: summary, event_description: event_description, location: location, link: link, start: start, end: end)
+                
+                self.events[dayCounter].append(newEvent)
+            }
         }
         
 //        let allResults: [NSDictionary] = results["items"] as! [NSDictionary]
@@ -116,6 +122,8 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
         UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
 
-
+    func handleSegment(daySegment: UISegmentedControl) {
+        println("sel = \(daySegment.selectedSegmentIndex)")
+        self.eventTableView.reloadData()
+    }
 }
-
